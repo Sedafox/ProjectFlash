6f6279932e4f22909a61d938e8635d84deee5b64
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-09-18 14:24:16 -0600

    Addedf basic movement to player object

--- Assets/Src/Scripts/WorldInteraction/PlayerController.cs
+++ Assets/Src/Scripts/WorldInteraction/PlayerController.cs
@@ -1,9 +1,15 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public int speed;
+    public int turnSpeed;
+     public float horizontalInput;
+    public float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMovement();
+    }
+
+      private void PlayerMovement()
+    { //abstraction
+       
+        
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+            verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+            
+         
         
     }
 }
